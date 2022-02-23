d66463ce7336339a22fedd8a118fa4ed7d67ff09
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-02-23 23:35:43 +0530

    Add files via upload

---
+++ Constructor demo.cpp
@@ -0,0 +1,31 @@
+// Constructor demo//
+// MAHADEV G I //
+
+#include<iostream>
+using namespace std;
+
+class demoConst
+{
+     private: int i=10;
+     public:
+             void setval()
+             {  
+                i=100;
+                cout<< "Setvalue() " << i << endl;
+             }
+             
+                demoConst()
+                {
+                   cout<< "Inside the constructor\n" << endl;
+                }
+};              
+
+int main()
+{
+    demoConst demo1, demo2;
+    demo1.setval();
+    demo2.setval();
+    
+return 0;
+}                    
+© 2022 GitHub, Inc.
